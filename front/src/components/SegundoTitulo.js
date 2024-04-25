0ec13d51f3827eb853245241f930831b4d10f50b
Juan <juan02pires@gmail.com>
2024-04-24 23:56:39 -0300

    style(front): altera o segundo componente de titulo

--- front/src/components/SegundoTitulo.js
+++ front/src/components/SegundoTitulo.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './css/SegundoTitulo.css'
 
+import { Link } from 'react-router-dom';
+  
 const SegundoTitulo = () => {
   return (
-    <h1 className='segundo-titulo'>FutEmoji</h1>
+    <h1 className='segundo-titulo'>
+      
+      <a>Fut</a>Em<span>⚽</span>ji
+      
+    </h1>
   )
 }
 
