0af08e238119b9229f3d329c2228e9babec69fd4
Juan <juan02pires@gmail.com>
2024-05-12 23:32:00 -0300

    refactor(back): modifica script do banco de dados

--- back/db_futemoji.sql
+++ back/db_futemoji.sql
@@ -2,6 +2,14 @@ CREATE DATABASE IF NOT EXISTS futemoji;
 
 USE futemoji;
 
+CREATE TABLE IF NOT EXISTS jogadores (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45) NOT NULL
+);
+
+INSERT INTO jogadores (nome) VALUES
+('Juan');
+
 CREATE TABLE IF NOT EXISTS campeonatos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL
@@ -15,15 +23,24 @@ INSERT INTO campeonatos (nome) VALUES
 ('Ligue 1'),
 ('Série A');
 
-CREATE TABLE IF NOT EXISTS nivel (
+CREATE TABLE IF NOT EXISTS pontuacao (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(13) NOT NULL
+    dificuldade VARCHAR(13) NOT NULL,
+    valor INT NOT NULL
 );
 
-INSERT INTO nivel (nome) VALUES
-('Fácil'),
-('Médio'),
-('Difícil');
+INSERT INTO pontuacao (dificuldade, valor) VALUES
+('Fácil', 1),
+('Médio', 2),
+('Difícil', 3);
 
+CREATE TABLE IF NOT EXISTS tabela_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_consulta DATETIME
+);
+
+
+SELECT * FROM jogadores;
 SELECT * FROM campeonatos;
-SELECT * FROM nivel;
+SELECT * FROM pontuacao;
+SELECT * FROM tabela_logs;
