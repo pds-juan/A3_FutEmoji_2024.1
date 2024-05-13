a915e7e6538737e7c659d7ec78f55aff20a2626e
Juan <juan02pires@gmail.com>
2024-05-12 23:36:44 -0300

    feat(back): cria o endpoint GET consultar BD

--- back/index.js
+++ back/index.js
@@ -1,10 +1,34 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import db from './db.js';
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/hello-world', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-    console.log('Porta 3000.')
+app.get('/consultar', async (req, res) => {
+    try {
+        const connection = await db;
+
+        await connection.execute('INSERT INTO tabela_logs (data_consulta) VALUES (?)', [new Date()]);
+
+        const [jogadores] = await connection.execute('SELECT * FROM jogadores');
+        const [campeonatos] = await connection.execute('SELECT * FROM campeonatos');
+        const [pontuacao] = await connection.execute('SELECT * FROM pontuacao');
+        const [logs] = await connection.execute('SELECT * FROM tabela_logs');
+
+        res.status(200).json({ jogadores, campeonatos, pontuacao, logs });
+    } catch (error) {
+        console.error('Erro ao realizar a consulta:', error);
+        res.status(500).send('Um erro inesperado ocorreu ao realizar a consulta.');
+    }
+});
+
+const porta = 3000;
+app.listen(porta, () => {
+    console.log(`Porta ${porta}.`)
 });
