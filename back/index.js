7218dc418c7606ff42d90a78b96ef05773fa6e95
Juan <juan02pires@gmail.com>
2024-05-26 11:58:35 -0300

    feat(back): cria endpoint POST salvar-nome

--- back/index.js
+++ back/index.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/salvar-nome', async (req, res) => {
+    const { nome } = req.body;
+
+    try {
+        const connection = await db;
+        await connection.execute('INSERT INTO jogadores (nome) VALUES (?)', [nome]);
+
+    } catch (error) {
+        console.log('Erro ao salvar o nome:', error);
+        res.status(500).send('Erro ao salvar o nome no banco de dados.');
+    }
+});
+
 app.post('/conversar', async (req, res) => {
     // const { prompt } = req.body;
 
@@ -17,7 +30,7 @@ app.post('/conversar', async (req, res) => {
     const role = 'user';
     const max_tokens = 50;
 
-    const promptCompleto = `Você vai me enviar um conjunto de 3 emojis, a partir desses emojis, preciso adivinhar o time de futebol que se trata. Porém, segue algumas regras: você NÃO deve utilizar emojis que possuem letras; você NÃO pode utilizar emojis que só possuem cores; seja criativo nos emojis; quero emojis que representem os times, não escolha emojis aleatórios; quero que utilize uma base de dados vasta de emojis, o céu é o limite; não quero repetição de times; tente sempre usar emojis diferentes para cada time.` // ${prompt}`;
+    const promptCompleto = `Você vai me enviar um conjunto de 3 emojis, que representam um time de futebol. Seguem algumas regras: você NÃO deve utilizar emojis que possuem letras; você NÃO pode utilizar emojis que só possuem cores; seja criativo nos emojis; quero emojis que representem os times, não use emojis aleatórios; quero que utilize uma base vasta de emojis, o céu é o limite; não quero repetição de times; use sempre emojis diferentes para cada time; escolha apenas times de campeonatos ingleses; APENAS UM TIME POR VEZ. A resposta deve ser exatamente no seguinte padrão, nada além disso: 'emoji 1''emoji 2''emoji 3''nome do time que os emojis representam'.` // ${prompt}`;
 
     const completion = await openai.chat.completions.create({
         messages: [{ role: role, content: promptCompleto }],
@@ -26,6 +39,9 @@ app.post('/conversar', async (req, res) => {
     });
 
     const resposta = completion.choices[0].message.content;
+    const respostaFiltrada = resposta.replace(/\s/g, '');
+    const emojis = respostaFiltrada.substring(0, 6);
+    const nomeTime = respostaFiltrada.substring(6);
 
     try {
         const connection = await db;
@@ -35,7 +51,7 @@ app.post('/conversar', async (req, res) => {
         res.json({ promptCompleto, resposta });
 
     } catch (error) {
-        console.error('Erro ao incluir as informações no banco de dados:', error);
+        console.log('Erro ao incluir as informações no banco de dados:', error);
         res.status(500).send('Um erro inesperado ocorreu ao incluir as informações no banco de dados.');
     }
 });
@@ -57,12 +73,12 @@ app.get('/consultar', async (req, res) => {
         res.json({ ultimaConversa });
 
     } catch (error) {
-        console.error('Erro ao realizar a consulta:', error);
+        console.log('Erro ao realizar a consulta:', error);
         res.status(500).send('Um erro inesperado ocorreu ao realizar a consulta.');
     }
 });
 
-const porta = 3000;
+const porta = 3001;
 app.listen(porta, () => {
     console.log(`Porta ${porta}.`)
 });
