30a47aa82562cc5861e6c72f4cbe98d1ab2b176e
Juan <juan02pires@gmail.com>
2024-05-25 23:52:34 -0300

    style(front): atualizacao na tela de emojis

--- front/src/components/AreaEmoji.js
+++ front/src/components/AreaEmoji.js
@@ -4,15 +4,9 @@ import './css/AreaEmoji.css'
 const AreaEmoji = () => {
     return (
         <div className="container-area-emoji">
-
-            <i class="fa-solid fa-arrow-left" id='seta-esquerda'></i>
-
             <div className='area-emoji'>
                 <p>🔴⚪⚫</p>
             </div>
-
-            <i class="fa-solid fa-arrow-right" id='seta-direita'></i>
-
         </div>
     )
 }
