0116a24e3ad145adb83cda8b18c03978d0697228
Juan <juan02pires@gmail.com>
2024-05-26 12:00:56 -0300

    feat(front): envia nome do jogador ao back

--- front/src/components/NomeJogador.js
+++ front/src/components/NomeJogador.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/NomeJogador.css'
 import { Link } from 'react-router-dom'
+import axios from '../axios'
 
 const NomeJogador = () => {
+    const [nome, setNome] = useState('');
+
+    const escrevendoNome = (event) => {
+        setNome(event.target.value);
+    };
+
+    const enviarNome = async () => {
+        try {
+            await axios.post('http://localhost:3001/salvar-nome',
+                { nome }
+            );
+        } catch (error) {
+            console.log('Erro ao salvar o nome:', error);
+        }
+    };
+
     return (
         <div className='container-jogador'>
             <h2 className='subtitulo-jogador'>Digite o Seu Nome:</h2>
 
             <div className="input-jogador">
-                <input type="text" placeholder='Seu nome aqui' />
+                <input type="text" placeholder='Seu nome aqui' value={nome} onChange={escrevendoNome} />
             </div>
 
-            <button className='botao-proximo'>
+            <button className='botao-proximo' onClick={enviarNome}>
                 <Link to="/configuracoes">
                     PRÓXIMO
                 </Link>
