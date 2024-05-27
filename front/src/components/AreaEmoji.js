1d43494417e2ae82b854e2da383714a7ac34d1aa
Juan <juan02pires@gmail.com>
2024-05-26 23:43:09 -0300

    feat(front): renderiza os emojis na tela

--- front/src/components/AreaEmoji.js
+++ front/src/components/AreaEmoji.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import './css/AreaEmoji.css'
+import React, { useState, useEffect } from 'react';
+import './css/AreaEmoji.css';
+import axios from '../axios';
 
 const AreaEmoji = () => {
+    const [emojis, setEmojis] = useState('');
+
+    useEffect(() => {
+        const pegarEmojis = async () => {
+            try {
+                const busca = await axios.get('/emojis');
+                setEmojis(busca.data.emojis);
+            } catch (error) {
+                console.log('Erro ao obter emojis:', error);
+            }
+        };
+        pegarEmojis();
+    }, []);
+
     return (
         <div className="container-area-emoji">
             <div className='area-emoji'>
-                <p>🔴⚪⚫</p>
+                <p>{emojis}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AreaEmoji
+export default AreaEmoji;
