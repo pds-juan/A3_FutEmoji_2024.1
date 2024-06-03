f87e4bd35bc3adbfdc1bfa731a6fe81dd6f5f50b
Juan <juan02pires@gmail.com>
2024-06-02 23:51:46 -0300

    feat(front): recebe a resposta do jogador

--- front/src/components/Resposta.js
+++ front/src/components/Resposta.js
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './css/Resposta.css'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from '../axios'
 
 const Resposta = () => {
+    const [resposta, setResposta] = useState('');
+    const [respostaCorreta, setRespostaCorreta] = useState('');
+    const [resultado, setResultado] = useState('');
+    const [tentativas, setTentativas] = useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const compararRespostas = async () => {
+            try {
+                const busca = await axios.get('/resposta');
+                setRespostaCorreta(busca.data.nomeTime);
+
+                if (!resposta) {
+                    setResultado("Digite a sua resposta!");
+                } else if (respostaCorreta.includes(resposta)) {
+                    setResultado("Resposta Correta!");
+                    if (tentativas < 3) {
+                        await axios.post('/conversar');
+                        window.location.reload();
+                        setTentativas(tentativas + 1);
+                    } else {
+                        alert("Parabéns! Você venceu!")
+                        navigate('/');
+                    }
+                } else {
+                    setResultado("Resposta Incorreta!");
+                    navigate('/');
+                }
+            } catch (error) {
+                console.log('Erro ao obter a resposta:', error);
+            }
+        };
+        compararRespostas();
+    });
+
     return (
         <div className='container-resposta'>
             <div className="campo-resposta">
-                <input type="text" placeholder='Digite sua resposta aqui' />
+                <input type="text" placeholder='Digite sua resposta aqui' onBlur={(event) => setResposta(event.target.value)} />
             </div>
 
-            <button className='botao-enviar'>
-                <Link>
-                    ENVIAR
-                </Link>
+            <button className='botao-enviar' onClick={() => alert(resultado)}>
+                ENVIAR
             </button>
         </div>
     )
