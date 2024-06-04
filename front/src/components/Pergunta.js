fcbe9c54bcf59bbd227a4480463a9cff408c9528
Juan <juan02pires@gmail.com>
2024-06-04 02:07:26 -0300

    feat(front): apresenta a dificuldade selecionada

--- front/src/components/Pergunta.js
+++ front/src/components/Pergunta.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css/Pergunta.css'
+import axios from '../axios';
 
 const Pergunta = () => {
+    const [dificuldade, setDificuldade] = useState('');
+
+    useEffect(() => {
+        const obterDificuldade = async () => {
+            try {
+                const busca = await axios.get('/obter-dificuldade');
+                setDificuldade(busca.data.dificuldade);
+            } catch (error) {
+                console.log('Erro ao obter a dificuldade:', error);
+            }
+        };
+        obterDificuldade();
+    });
+
     return (
         <div className='container-pergunta'>
             <p>Qual é o Time?</p>
-            <p>Dificuldade: Fácil</p>
+            <p>Dificuldade: {dificuldade}</p>
         </div>
     )
 }
