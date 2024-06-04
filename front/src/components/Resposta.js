f596269ae8fca0585511761395907863348a7bb0
Juan <juan02pires@gmail.com>
2024-06-03 23:54:23 -0300

    feat(front): compara respostas e finaliza o jogo

--- front/src/components/Resposta.js
+++ front/src/components/Resposta.js
@@ -7,7 +7,6 @@ const Resposta = () => {
     const [resposta, setResposta] = useState('');
     const [respostaCorreta, setRespostaCorreta] = useState('');
     const [resultado, setResultado] = useState('');
-    const [tentativas, setTentativas] = useState(0);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -17,20 +16,21 @@ const Resposta = () => {
                 setRespostaCorreta(busca.data.nomeTime);
 
                 if (!resposta) {
-                    setResultado("Digite a sua resposta!");
+                    setResultado("Digite sua resposta!");
                 } else if (respostaCorreta.includes(resposta)) {
-                    setResultado("Resposta Correta!");
-                    if (tentativas < 3) {
-                        await axios.post('/conversar');
-                        window.location.reload();
-                        setTentativas(tentativas + 1);
-                    } else {
-                        alert("Parabéns! Você venceu!")
-                        navigate('/');
+                    setResultado("Resposta correta!\nParabéns, você venceu!");
+                    if (resultado) {
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 1000);
                     }
                 } else {
-                    setResultado("Resposta Incorreta!");
-                    navigate('/');
+                    setResultado("Resposta incorreta.\nFim de jogo!");
+                    if (resultado) {
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 1000);
+                    }
                 }
             } catch (error) {
                 console.log('Erro ao obter a resposta:', error);
@@ -42,7 +42,7 @@ const Resposta = () => {
     return (
         <div className='container-resposta'>
             <div className="campo-resposta">
-                <input type="text" placeholder='Digite sua resposta aqui' onBlur={(event) => setResposta(event.target.value)} />
+                <input type="text" placeholder='Digite sua resposta aqui' onBlur={(event) => setResposta(event.target.value.replace(/\s/g, '').normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase())} />
             </div>
 
             <button className='botao-enviar' onClick={() => alert(resultado)}>
